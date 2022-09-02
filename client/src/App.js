f79731076b58c30174a48d37931c4cd46cdecb78
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-09-02 11:10:15 +0100

    moved static file

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import SettingsDial from './components/SettingsDial';
 import SpinWheel from './containers/SpinWheel';
 import Dice from './components/Dice';
 import HL from './components/HL';
-import background from "../.public/static/app-bg.png"
+
 
 
 
@@ -113,7 +113,7 @@ function App() {
   return (
     <div className='app-wrapper'>
 
-      <img src={background} alt="" className='bg' />
+      <img src="static/app-bg.png" alt="" className='bg' />
       <div className="app">
         <Navbar user={user} updateMoney={updateMoney} />
 
