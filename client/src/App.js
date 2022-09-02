7e4899fe2addd940af50f1fe206f77a2ada89792
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-09-02 11:13:50 +0100

    more change

--- client/src/App.js
+++ client/src/App.js
@@ -113,7 +113,7 @@ function App() {
   return (
     <div className='app-wrapper'>
 
-      <img src="static/app-bg.png" alt="" className='bg' />
+      <img src="/BLACKJACK_Group_Project/client/src/static/app-bg.png" alt="" className='bg' />
       <div className="app">
         <Navbar user={user} updateMoney={updateMoney} />
 
